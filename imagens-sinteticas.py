413960655be97305869e98518085efb589829248
Izabella Terencio <izahterencio@gmail.com>
2020-10-21 19:08:01 -0300

    Sintetizando imagens: bandeira da França

--- imagens-sinteticas.py
+++ imagens-sinteticas.py
@@ -24,7 +24,23 @@ def triangulo(size):
                 image.putpixel((x,y), preto)
     return image
 
+#para desenhar figura semelhante à bandeira da França: retângulo com três listras
+def bandeira_franca(altura):
+    azul = (0,85,164)
+    branco = (255,255,255)
+    vermelho = (239,65,53)
+    largura = 3*altura//2 #// é operação de divisão inteira
+
+    image = Image.new("RGB",(largura,altura),branco) #a imagem inicia toda branca
+    intervalo = largura//3
+
+    for x in range(intervalo):
+        for y in range(altura):
+            image.putpixel((x,y),azul)
+            image.putpixel((x+2*intervalo,y),vermelho)
+    return image
+
 #para ser executado como main
 if __name__ == "__main__":
-    t = triangulo(700)
+    t = bandeira_franca(700)
     t.show()
